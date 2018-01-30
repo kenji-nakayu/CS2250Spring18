3d6c9d4ac0fff2acc7083467a28c818cbdf2ecb2
kenji-nakayu <kenji.nakayu@gmail.com>
2018-01-30 09:42:58 -0700

    Updating file

--- module4/guess.c
+++ module4/guess.c
@@ -29,10 +29,10 @@ int main()
     int input1, input2, input3, guess;
 
     srand(time(0));
-    guess = ( rand() % 10) + 1
+    guess = ( rand() % 10) + 1;
 
     printf("Guess a number between 1 and 10\n");
-    scanf("%d", &input);
+    scanf("%d", &input1);
 
     if(input1 == guess){
         printf("You won! The number was %d!\n", guess);
@@ -41,7 +41,7 @@ int main()
         printf("Your guess was wrong. You guessed %d.\nGuess another number between 1 and 10:\n", input1);
         scanf("%d", &input2);
         if(input2 == input1){
-            printf("You already guessed %d.\nGuess a number between 1 and 10 other than %d:", input1, input1);
+            printf("You already guessed %d.\nGuess a number between 1 and 10 other than %d:\n", input1, input1);
             scanf("%d", &input2);
         }
         if(input2 == guess){
