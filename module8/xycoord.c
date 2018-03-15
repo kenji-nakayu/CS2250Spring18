ed4e71c44881189cadb6bc7fe92c19dff0a9e5b9
Kenji Nakaju <kenjinakaju@mail.weber.edu>
2018-03-15 09:01:16 -0600

    Initial commit

---
+++ module8/xycoord.c
@@ -0,0 +1,64 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  xycoord.c
+ *
+ *    Description:  Function definitions for the
+ *                  x y coordinate file
+ *
+ *        Version:  1.0
+ *        Created:  03/15/2018 08:51:00 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Kenji Nakaju (), kenjinakaju@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include "xycoord.h"
+
+// Function Definitions
+// 1) Initialize your Point to (0, 0)
+void InitPoint(int* xa, int* ya)
+{
+    printf("Beg InitPoint\n");
+    // To update the values at a given address
+    // use the de-reference operator "*"
+    ShowPoint(*xa, *ya); // values of xa and ya
+    // Update the values
+    *xa = 0;
+    *ya = 0;
+    ShowPoint(*xa, *ya);
+    printf("End InitPoint\n");
+    return;
+}
+// 2) Display current Point position
+void ShowPoint(int x, int y)
+{
+    printf("X-Position = [%d]\n", x);
+    printf("Y-Position = [%d]\n", y);
+    return;
+}
+// 3) Move/Update your Point position
+//  pass newx and newy by value
+//  pass xa and ya by reference
+void MovePoint(int newx, int newy, int* xa, int* ya) 
+{
+    *xa = newx;
+    *ya = newy;
+    return;
+}
+
+// Update Point. You ask user to enter new values
+void UpdatePoint(int* xa, int* ya)
+{
+    printf("Enter your x-coordinate:");
+    // When using an address/pointer, do not use
+    // the Address "&" operator
+    scanf("%d",xa);
+    printf("Enter your y-coordinate:");
+    scanf("%d",ya);
+    return;
+}
---
+++ module8/xycoord.h
@@ -0,0 +1,39 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  xycoord.h
+ *
+ *    Description:  Header file for the functions used in
+ *                  x y coordinate program
+ *
+ *        Version:  1.0
+ *        Created:  03/15/2018 08:50:44 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Kenji Nakaju (), kenjinakaju@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+
+#ifndef _XY_COORD_H_
+#define _XY_COORD_H_
+
+// Constants
+static const int X = 0;
+static const int Y = 1;
+static const int DIM = 2;
+// Function Prototypes
+// 1) Initialize your Point to (0, 0)
+void InitPoint(int* xa, int* ya); // pass by reference
+// 2) Display current Point position
+void ShowPoint(int x, int y); // pass by value
+// 3) Move/Update your Point position
+//  pass newx and newy by value
+//  pass xa and ya by reference
+void MovePoint(int newx, int newy, int* xa, int* ya); 
+// Update Point. You ask user to enter new values
+void UpdatePoint(int* xa, int* ya); // pass by reference
+
+#endif
